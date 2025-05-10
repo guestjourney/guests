8e76437d63202beb9f9fe9295ea694b962054bdb
orel hindi <orelhindi@orels-MacBook-Pro.local>
2025-05-11 02:49:35 +0300

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import errorAnimation from "@/lotties/error.json";
+import dynamic from "next/dynamic";
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+
+export default function NotFound({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center p-4">
+      <Lottie
+        animationData={errorAnimation}
+        loop={true}
+        className="w-48 h-48"
+      />
+      <h2 className="text-2xl font-bold mb-2">Page Not Found</h2>
+      <p className="text-center text-gray-500 mb-4">
+        The page you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
